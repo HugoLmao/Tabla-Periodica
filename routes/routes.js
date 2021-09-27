f84d0b6e4e4461099d1adefa80e163c46ede550c
HugoLmao <89503817+HugoLmao@users.noreply.github.com>
2021-09-27 13:09:48 -0500

    Hechas rutas para los controladores.

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,59 @@
 const express = require('express');
 
-const router = express.Router();
+const enrutador = express.Router();
 
-module.exports = router;
+const element = require('../controllers/elements')
+const type = require('../controllers/types')
+const period = require('../controllers/periods')
+const group = require('../controllers/groups')
+
+
+//Rutas para elementos
+
+enrutador
+    .route("/elements")
+    .get(element.getElements)
+    .post(element.createElement);
+    
+enrutador
+    .route("/elements/:id")
+    .put(element.updateElement)
+    .delete(element.deleteElement);
+
+    //Rutas para tipos
+
+enrutador
+    .route("/types")
+    .get(type.getTypes)
+    .post(type.createType);
+    
+enrutador
+    .route("/types/:id")
+    .put(type.updateType)
+    .delete(type.deleteType);
+    
+//Rutas para periodos
+
+enrutador
+.route("/periods")
+    .get(period.getPeriods)
+    .post(period.createPeriod);
+    
+enrutador
+    .route("/periods/:id")
+    .put(period.updatePeriod)
+    .delete(period.deletePeriod);
+
+//Rutas para grupos
+
+enrutador
+    .route("/groups")
+    .get(group.getGroups)
+    .post(group.createGroup);
+
+enrutador
+    .route("/groups/:id")
+    .put(group.updateGroup)
+    .delete(group.deleteGroup);
+
+module.exports = enrutador;
